b6218a3a755e3dbec3912a53331fd755e977468c
Luiscvj <Luiscarlos.villanjurado@gmail.com>
2024-06-17 08:56:58 -0500

    feat: Global configuration

--- AwpProject/src/app/app.config.ts
+++ AwpProject/src/app/app.config.ts
@@ -2,11 +2,18 @@ import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { AuthInterceptorService } from './Services/User/auth-interceptor.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
                provideRouter(routes,withComponentInputBinding()),
-               importProvidersFrom(HttpClientModule), provideAnimationsAsync()]
+               importProvidersFrom(HttpClientModule), provideAnimationsAsync(),
+               {
+                  provide:HTTP_INTERCEPTORS,
+                  useClass:AuthInterceptorService,
+                  multi: true
+                }
+             ]
 };
--- AwpProject/src/app/app.routes.ts
+++ AwpProject/src/app/app.routes.ts
@@ -4,6 +4,10 @@ import { RegisterComponent } from './Pages/register/register.component';
 import { HomeComponent } from './Pages/home/home.component';
 import { authGuard } from './Guards/auth.guard';
 import { OffcanvasComponent } from './Pages/offcanvas/offcanvas.component';
+import { ProjectsComponent } from './Pages/home/ChildComponents/projects/projects.component';
+import { OverallProjectComponent } from './Pages/home/ChildComponents/overall-project/overall-project.component';
+import { WorkBreakdownStructureComponent } from './Pages/home/ChildComponents/work-breakdown-structure/work-breakdown-structure.component';
+import { CreditRulesComponent } from './Pages/home/ChildComponents/credit-rules/credit-rules.component';
 
 
 export const routes: Routes = 
@@ -11,5 +15,14 @@ export const routes: Routes =
     {path:'',component:LoginComponent},
     {path:'login',component:LoginComponent},
     {path:'register',component:RegisterComponent},
-    {path:'home', component:HomeComponent, canActivate:[authGuard]}//determines if a route can be activate
+    {path:'home',
+     component:HomeComponent, 
+     children:
+     [
+        {path: '',component:ProjectsComponent},
+        {path:'projects',component:ProjectsComponent},
+        {path:'overall-project',component:OverallProjectComponent},
+        {path:'work-breakdown-structure/:projectId', component:WorkBreakdownStructureComponent},
+        {path:'credit-rules/:projectId',component:CreditRulesComponent}
+     ]/*canActivate:[authGuard]*/}//determines if a route can be activate
 ];
